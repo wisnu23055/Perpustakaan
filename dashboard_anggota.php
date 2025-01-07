bd282d74b0e6cdf0b7e52b378f472a3aef8f5da1
Wisnu Prabowo <148554858+wisnu23055@users.noreply.github.com>
2025-01-07 14:22:00 +0700

    Update dashboard_anggota.php

--- dashboard_anggota.php
+++ dashboard_anggota.php
@@ -79,6 +79,7 @@ if (isset($_POST['pinjam'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Anggota</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="icon" href="assets/image/logo.jpg" type="image/jpg">
     <style>
         body {
             font-family: 'Poppins', sans-serif;
