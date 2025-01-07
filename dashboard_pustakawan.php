b5bc98fc806a0fd62b69261841822434ffe5edad
Wisnu Prabowo <148554858+wisnu23055@users.noreply.github.com>
2025-01-07 14:21:34 +0700

    Update dashboard_pustakawan.php

--- dashboard_pustakawan.php
+++ dashboard_pustakawan.php
@@ -100,6 +100,7 @@ $peminjaman_result = mysqli_query($koneksi, $query_peminjaman);
     <!-- Tambahkan Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="icon" href="assets/image/logo.jpg" type="image/jpg">
     <!-- Tambahkan CSS -->
     <style>
         body {
