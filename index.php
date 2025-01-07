18488695b3345c52b0fa767cde68729c08c2300f
Wisnu Prabowo <148554858+wisnu23055@users.noreply.github.com>
2025-01-07 14:16:26 +0700

    Update index.php
    add a logo web

--- index.php
+++ index.php
@@ -91,6 +91,7 @@ if (isset($_POST['register_anggota'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perpustakaan</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="icon" href="assets/image/logo.jpg" type="image/jpg">
     <style>
         /* Reset CSS */
         * {
